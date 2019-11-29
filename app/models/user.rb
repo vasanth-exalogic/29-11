79cd99805197f5ccbb710862998b5fc7d7f80f9f
vasanth-exalogic <vasanth@exalogic.in>
2019-11-29 18:14:31 +0530

    2 commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :lname, format: {with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/}
   validates :ename, format: {with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/}
   validates_uniqueness_of :email
+  validates :gender, presence: true
 end
